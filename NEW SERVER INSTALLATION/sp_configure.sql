8dd0b900d748f8ce99a76e3c8b08a0ab081d9c7c
vladimir-aleyev <74710973+vladimir-aleyev@users.noreply.github.com>
2021-06-02 16:13:11 +0300

    Update sp_configure.sql

--- NEW SERVER INSTALLATION/sp_configure.sql
+++ NEW SERVER INSTALLATION/sp_configure.sql
@@ -7,22 +7,22 @@ exec sp_configure 'Ad Hoc Distributed Queries', 1;
 reconfigure;
 exec sys.sp_configure N'backup checksum default', N'1'
 RECONFIGURE WITH OVERRIDE
--- ”становить сжатие резервных копий по умолчанию:
+
 EXEC sys.sp_configure N'backup compression default', N'1'
 GO
 RECONFIGURE WITH OVERRIDE
 GO
--- активировать опцию Сoptimize for ad hoc workloadsТ:
+
 EXEC sys.sp_configure N'optimize for ad hoc workloads', N'1'
 GO
 RECONFIGURE WITH OVERRIDE
 GO
--- установить возможность удаленного Сdedicated administrator connection (DAC)Т:
+
 EXEC sp_configure 'remote admin connections', 1;
 GO
 RECONFIGURE WITH OVERRIDE
 GO
--- активировать возможность использовани€ 'Database Mail' на сервере:
+
 EXEC sp_configure 'Database Mail XPs', 1;
 GO
 RECONFIGURE
